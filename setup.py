9127701a84a892a2b1c694f6d7939a780b1ad772
Samiksha <samiksha@Tanishqs-MacBook-Pro.local>
2026-06-03 13:13:28 +0530

    Intial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="knowledge RAG",
+    version="0.1.0",
+    author="Samiksha Srivastava",
+    author_email="samikshasrivastava607@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
